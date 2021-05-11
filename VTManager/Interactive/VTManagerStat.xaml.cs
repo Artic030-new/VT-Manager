7041a36de2bed37a0b57ee30939e764c5dcf6d4d
Artic030 <030ci@mail.ru>
2021-05-12 01:17:49 +0300

    Add VM VTManagerStat

---
+++ VTManager/Interactive/VTManagerStat.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VTManager.Interactive
+{
+    /// <summary>
+    /// Логика взаимодействия для VTManagerStat.xaml
+    /// </summary>
+    public partial class VTManagerStat : UserControl, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public VTManagerStat()
+        {
+            InitializeComponent();
+        }
+
+        public void NotifyPropertyChanged(string info) {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+        }
+
+        private string _value;
+        public string ItemValue {
+            get { return _value; } 
+            set { _value = value; }
+        }
+        private string _target;
+        public string ItemTarget {
+            get { return _target; }
+            set { _target = value; }
+        }
+    }
+}
