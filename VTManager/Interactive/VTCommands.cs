956ded28d1524fb486f9efd6720c3b98ac76b5dc
Artic030 <030ci@mail.ru>
2021-05-17 09:45:56 +0300

    Abstract command class

--- VTManager/Interactive/VTCommands.cs
+++ VTManager/Interactive/VTCommands.cs
@@ -7,31 +7,20 @@ using System.Windows.Input;
 
 namespace VTManager.Interactive
 {
-    class VTCommands : ICommand
+    internal abstract class VTCommands : ICommand
     {
         private Action<object> execute;
         private Func<object, bool> canExecute;
 
         public event EventHandler CanExecuteChanged
         {
-            add { CommandManager.RequerySuggested += value; }
-            remove { CommandManager.RequerySuggested -= value; }
+            add => CommandManager.RequerySuggested += value; 
+            remove => CommandManager.RequerySuggested -= value; 
         }
 
-        public VTCommands(Action<object> execute, Func<object, bool> canExecute = null)
-        {
-            this.execute = execute;
-            this.canExecute = canExecute;
-        }
+        public abstract bool CanExecute(object par);
 
-        public bool CanExecute(object parameter)
-        {
-            return this.canExecute == null || this.canExecute(parameter);
-        }
-
-        public void Execute(object parameter)
-        {
-            this.execute(parameter);
-        }
+        public abstract void Execute(object par);
+        
     }
 }
